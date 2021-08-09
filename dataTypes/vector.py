216934c4646d7299d21fec28b1fd26bc1a82172a
leonardoAchaBoiano <leonardo.acha@ucb.edu.bo>
2021-08-09 10:06:39 -0400

    09/08/21 class dismissed

--- dataTypes/vector.py
+++ dataTypes/vector.py
@@ -1,27 +1,37 @@
 class Vector(object):
-    def __init__(self):
-        pass
-        
+    def __init__(self, n=0, x=None):
+        if n >= 0:
+            self._data = [x for i in range(n)]
+        else:
+            raise Exception("Numero invalido de elementos")
+
+    def __repr__(self):
+        return str(self._data)
+
+    def __str__(self):
+        return str(f"Contenido: {str(self._data)}")
+
     def add(self, n):
-        pass
-    
+        self._data.append(n)
+        return self
+
     def insert(self, i, n):
-        pass
-    
+        self._data.insert(i, n)
+
     def remove(self, n):
         pass
-    
-    def get(self,i):
+
+    def get(self, i):
         pass
-    
+
     def set(self, i, n):
         pass
-    
+
     def size(self):
         pass
-    
+
     def isEmpty(self):
         pass
-    
+
     def clear(self):
         pass
