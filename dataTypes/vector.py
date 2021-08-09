a3459fd18f9ef9f27bc7c596578eaaa353aa2626
leonardoAchaBoiano <leonardo.acha@ucb.edu.bo>
2021-08-09 08:57:21 -0400

    Clase del lunes 09/08/21

---
+++ dataTypes/vector.py
@@ -0,0 +1,27 @@
+class Vector(object):
+    def __init__(self):
+        pass
+        
+    def add(self, n):
+        pass
+    
+    def insert(self, i, n):
+        pass
+    
+    def remove(self, n):
+        pass
+    
+    def get(self,i):
+        pass
+    
+    def set(self, i, n):
+        pass
+    
+    def size(self):
+        pass
+    
+    def isEmpty(self):
+        pass
+    
+    def clear(self):
+        pass
