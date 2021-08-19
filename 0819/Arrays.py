df74d46296cb7f43551a2f7bd1b65160b5201b8f
Francisco Suarez <fsuarez@ucb.edu.bo>
2021-08-19 09:11:29 -0400

    Array 2D and Matrix

--- 0819/Arrays.py
+++ 0819/Arrays.py
@@ -1,4 +1,46 @@
-from array1D import Array
+import ctypes 
+
+class Array:
+    def __init__(self, n):
+        assert n > 0
+        self._n = n
+        PyArrayType = ctypes.py_object * n
+        self._elementos = PyArrayType()
+        self.clear(None)
+
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self, i):
+        assert i >= 0 and i < len(self)
+        return self._elementos[i]
+
+    def __setitem__(self, i, v):
+        assert i >= 0 and i < len(self)
+        self._elementos[i] = v
+
+    def clear(self, v):
+        for i in range(len(self)):
+            self._elementos[i] = v
+
+    def __iter__(self):
+        return _ArrayIterator(self._elementos)
+
+class _ArrayIterator:
+    def __init__(self, arreglo):
+        self._arregloRef = arreglo
+        self._iActual = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._iActual < len(self._arregloRef):
+            elemento = self._arregloRef[self._iActual]
+            self._iActual += 1
+            return elemento
+        else:
+            raise StopIteration
 
 class Array2D:
     def __init__(self, nRows, nCols):
