6ef162c7f5a70e88683acc9983d63784dd36ba5b
Francisco Suarez <fsuarez@ucb.edu.bo>
2021-10-28 09:14:03 -0400

    Added recursive binary search

--- 1025/recursion.py
+++ 1025/recursion.py
@@ -30,4 +30,23 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-fibonacci(6)
+# fibonacci(6)
+
+def recBinarySearch(objetivo, secuencia, inferior, superior):
+    if inferior > superior: # caso base 1
+        return False
+    else:
+        medio = (inferior + superior) // 2
+        if secuencia[medio] == objetivo:
+            return True # caso base 2
+        elif objetivo < secuencia[medio]: # caso recursivo
+            return recBinarySearch(objetivo, secuencia, inferior, medio - 1)
+        else:
+            return recBinarySearch(objetivo, secuencia, medio + 1, superior)
+
+sec1 = [2, 4, 5, 10, 13, 21, 28, 34, 42, 50]
+recBinarySearch(42, sec1, 0, len(sec1)-1)
+
+# recBinarySearch(objetivo, secuencia, 0, 9) -> True
+    # recBinarySearch(objetivo, secuencia, 5, 9) -> True
+        # recBinarySearch(objetivo, secuencia, 8, 9) -> True
